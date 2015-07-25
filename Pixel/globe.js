a797ac6c392c2e7415d820238b9a53dfd945a9bd
James Bailey <jimi.bailey@gmail.com>
2015-07-25 10:16:31 +0100

    Added easing functions. Popup fades out instead of simply hiding.

--- Pixel/globe.js
+++ Pixel/globe.js
@@ -1,6 +1,34 @@
 var DAT = DAT || {};
 var initZoom, initCard;
 
+Easing = {
+  // no easing, no acceleration
+  linear: function (t) { return t },
+  // accelerating from zero velocity
+  easeInQuad: function (t) { return t*t },
+  // decelerating to zero velocity
+  easeOutQuad: function (t) { return t*(2-t) },
+  // acceleration until halfway, then deceleration
+  easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
+  // accelerating from zero velocity 
+  easeInCubic: function (t) { return t*t*t },
+  // decelerating to zero velocity 
+  easeOutCubic: function (t) { return (--t)*t*t+1 },
+  // acceleration until halfway, then deceleration 
+  easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+  // accelerating from zero velocity 
+  easeInQuart: function (t) { return t*t*t*t },
+  // decelerating to zero velocity 
+  easeOutQuart: function (t) { return 1-(--t)*t*t*t },
+  // acceleration until halfway, then deceleration
+  easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
+  // accelerating from zero velocity
+  easeInQuint: function (t) { return t*t*t*t*t },
+  // decelerating to zero velocity
+  easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
+  // acceleration until halfway, then deceleration 
+  easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
+}
 
 DAT.Globe = function(container, opts) {
   opts = opts || {};
@@ -578,15 +606,17 @@ DAT.Globe = function(container, opts) {
 		  $overlay.fadeIn();
 		  $popup.css({left:xStart, top:yStart }).show().animate({state:1},{
 			  progress : function( target, progress, time ){
+				  progress = Easing.easeInOutQuad( progress );
+				  
 				  var pnt = path.getPointAtLength( path.getTotalLength() * progress );
 				  var scale = 0.2 + 0.8 * progress;
 				  var transform = "scale("+scale+","+scale+")";
-				  $popup.css({left:pnt.x, top:pnt.y, "transform" : transform, opacity: progress });
+				  $popup.css({left:pnt.x, top:pnt.y, "transform" : transform, opacity: Math.min(1, 2 * progress) });
 			  },
 			  complete : function(){
 				  $( path ).remove();
 			  },
-			  duration : 1000
+			  duration : 800
 		  });
   	    }
   	    
@@ -596,7 +626,7 @@ DAT.Globe = function(container, opts) {
   }
 
  	$('#popup .close, #overlay').on('touchend, click', function(){
- 		$('#popup, #overlay').hide();
+ 		$('#popup, #overlay').fadeOut();
  	});
 
   
